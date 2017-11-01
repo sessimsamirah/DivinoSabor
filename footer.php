46dfdcb9af95747b296a5b7ac293c40e9eb3426d
Campus Osório <Campus Osrio@OSO-000000>
2017-11-01 09:19:53 -0200

    Inserindo o bgl

---
+++ footer.php
@@ -0,0 +1,13 @@
+<html> 
+    <head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.green-light_green.min.css" />
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> 
+    </head>
+
+    <body> 
+        
+    </body>
+    
+    
+</html>
