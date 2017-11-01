4d7c128c2166617ec5fd7dbf8fcbb17f40f80bae
Campus Osório <Campus Osrio@OSO-000000>
2017-11-01 11:13:47 -0200

    Atualizando rodapé

--- footer.php
+++ footer.php
@@ -6,7 +6,44 @@
     </head>
 
     <body> 
+        <footer class="mdl-mega-footer">
+  <div class="mdl-mega-footer__middle-section">
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading">Empresa</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">Contato</a></li> <!--Linkar pra página com o Tawk To -->
         
+      </ul>
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading">Redes Sociais</h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading">Localização</h1>
+      <ul class="mdl-mega-footer__link-list">
+          <p>LALALAALLA</p> <!-- Podemos inserir a API do Google Maps-->
+      </ul>
+    </div>
+
+  </div>
+
+  <div class="mdl-mega-footer__bottom-section" >
+    <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">LOGO DA WEDE</a></li>
+    </ul>
+  </div>
+
+</footer>
     </body>
     
     
