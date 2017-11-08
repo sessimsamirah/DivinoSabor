1d7a9f06c6ac37ef536fd7bbc1d2bd946c759453
Campus Osório <Campus Osrio@OSO-000000>
2017-11-08 09:38:05 -0200

    Corpo do Cardápio

--- cardapio.php
+++ cardapio.php
@@ -3,15 +3,93 @@
 <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.green-light_green.min.css" />
 <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> 
 
-
+<?php
+   include 'head.php';
+    ?>  
 <body> 
 
-   <?php
-   include 'head.php';
-   include 'footer.php';
    
-   ?>
+    
+    
+<div class="mdl-grid">
+  <div class="mdl-cell mdl-cell--4-col"><style>
+.demo-card-square.mdl-card {
+  margin-top: 150px;
+  width: 320px;
+  height: 320px;
+}
+.demo-card-square > .mdl-card__title {
+  color: #fff;
+  background:
+    url('../assets/demos/dog.png') bottom right 15% no-repeat #46B6AC;
+}
+</style>
+
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand">
+    <h2 class="mdl-card__title-text">Update</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Aenan convallis.
+  </div>
+</div>
+  </div>
+  <div class="mdl-cell mdl-cell--4-col"><style>
+.demo-card-square.mdl-card {
+  margin-top: 150px;
+  width: 320px;
+  height: 320px;
+}
+.demo-card-square > .mdl-card__title {
+  color: #fff;
+  background:
+    url('../assets/demos/dog.png') bottom right 15% no-repeat #46B6AC;
+}
+</style>
+
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand">
+    <h2 class="mdl-card__title-text">Update</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Aenan convallis.
+  </div>
+</div>
+  </div></div>
+  
+    
+    <div class="mdl-cell mdl-cell--4-col"><style>
+.demo-card-square.mdl-card {
+  margin-top: 150px;
+  width: 320px;
+  height: 320px;
+}
+.demo-card-square > .mdl-card__title {
+  color: #fff;
+  background:
+    url('../assets/demos/dog.png') bottom right 15% no-repeat #46B6AC;
+}
+</style>
+
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand">
+    <h2 class="mdl-card__title-text">Update</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Aenan convallis.
+  </div>
+</div>
+  </div>
+
+ 
+
+ 
 </body>
 
-    
+    <?php 
+       include 'footer.php';
+    ?>  
 </html> 
