d01f1b7ceeebee4cf42c273186d5c7abe5e6136c
Campus Osório <Campus Osrio@OSO-000000>
2017-11-08 08:22:37 -0200

    Atualizando menu

--- head.php
+++ head.php
@@ -32,7 +32,7 @@
     <!-- Tabs -->
     <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
       <a href="#scroll-tab-1" class="mdl-layout__tab is-active">Home</a>
-      <a href="#scroll-tab-2" class="mdl-layout__tab">Portfólio</a>
+      <a href="#scroll-tab-2" class="mdl-layout__tab">Cardápio</a>
       <a href="#scroll-tab-4" class="mdl-layout__tab">Carrinho</a>
     </div>
   </header>
