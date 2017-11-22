07ef9ba6b5a6b995885deeacd985cf64f982e10a
Dell <Dell@Dell-PC>
2017-11-22 20:47:40 -0200

    Dando includes no index

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.green-light_green.min.css" />
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> 
+
+<?php
+include 'head.php';
+?>  
+<?php
+include 'footer.php';
+?>
