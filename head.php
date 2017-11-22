5632bb420a546e92ed40dd05717ad99f629381f4
Campus Osório <Campus Osrio@OSO-000000>
2017-11-22 11:18:39 -0200

    comitando cabeçalho novo

--- head.php
+++ head.php
@@ -1,64 +1,41 @@
 
 
- 
 <?php
 echo '
-
-        <!-- Simple header with scrollable tabs. -->
-        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header" >
-  <header class="mdl-layout__header" style="position: fixed;">
-      <!-- Expandable Textfield -->
-   
-
-    <div class="mdl-layout__header-row">
-       
-      <!-- Title -->
-      <span class="mdl-layout-title">Divino Sabor (LOGO)</span>
-       <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-            mdl-textfield--floating-label mdl-textfield--align-right" style="margin-left: 80%; position: fixed; ">
-        <label class="mdl-button mdl-js-button mdl-button--icon"
-               for="fixed-header-drawer-exp">
-          <i class="material-icons">search</i>
-        </label>
-        <div class="mdl-textfield__expandable-holder">
-          <input class="mdl-textfield__input" type="text" name="sample"
-                 id="fixed-header-drawer-exp">
-        </div>
-      </div>
-    </div> 
-      
-    <!-- Tabs -->
-    <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-      <a href="#" class="mdl-layout__tab">Home</a>
-      <a href="cardapio.php" class="mdl-layout__tab">Cardápio</a>
-      <a href="#" class="mdl-layout__tab">Orçamento</a>
-    </div>
-  </header>
-  
-  <main class="mdl-layout__content">
-    <section class="mdl-layout__tab-panel is-active" id="scroll-tab-1">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-2">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-3">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-4">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-5">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-    <section class="mdl-layout__tab-panel" id="scroll-tab-6">
-      <div class="page-content"><!-- Your content goes here --></div>
-    </section>
-  </main>
-</div>
-  
-'; 
-
-?> 
-
     
+
+         <style>
+       /* Style the tab buttons */
+.tablink {
+    background-color: #7AA55D;
+    color: white;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    font-size: 17px;
+    width: 25%;
+}
+
+/* Change background color of buttons on hover */
+.tablink:hover {
+    background-color: #777;
+}
+
+/* Set default styles for tab content */
+.tabcontent {
+    color: white;
+    display: none;
+    padding: 50px;
+    text-align: center;
+}
+
+        </style> 
+
+<button class="tablink">LOGO</button>
+<a href="index.php"><button class="tablink" onclick="openCity("Paris", this, "green">Home</button></a>
+<a href="cardapio.php"><button class="tablink" onclick="openCity("Paris", this, "green")">Cardápio</button></a>
+<button class="tablink" onclick="openCity("Paris", this, "green")">Orçamento</button> 
+'; 
+?>
