5e4856ebaaba5fe4737de921e499b0801ae4c33f
Samirah Sessim <32516708+sessimsamirah@users.noreply.github.com>
2017-11-28 18:18:07 -0200

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -5,18 +5,14 @@ $username = "root";
 $password = "";
 $dbname = "bancods";
 
-//conexão com o servidor
 $conn = mysql_connect("localhost", "root", "");
 
-// Caso a conexão seja reprovada, exibe na tela uma mensagem de erro
 if (!$conn) {
     die("Falha na coneco com o Banco de Dados!");
 }
 
-// Caso a conexão seja aprovada, então conecta o Banco de Dados.    
 $db = mysql_select_db("bancods");
 
-/*Configurando este arquivo, depois é só você dar um include em suas paginas php, isto facilita muito, pois caso haja necessidade de mudar seu Banco de Dados
-você altera somente um arquivo*/
+
 ?>
 
