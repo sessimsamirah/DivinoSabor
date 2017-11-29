a8a22c4a9f76d27d0674001c0718b1d3e53d6bc7
Samirah Sessim <32516708+sessimsamirah@users.noreply.github.com>
2017-11-29 16:03:24 -0200

    galeria definitiva

--- cardapio.php
+++ cardapio.php
@@ -6,21 +6,98 @@
 
 <?php
    include 'head.php';
+   include 'conecta.php';
       ?> 
 
 
 <body> 
+<?php 
+$sql = "SELECT nomeProduto, descricao FROM produto";
+$resultado = mysqli_query($conn, $sql); 
 
-$consulta = "SELECT * FROM fotos where data='$datas' and status='Sim' order by id LIMIT $inicio, $qnt";
+if (mysqli_num_rows($resultado) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($resultado)) {
+        echo " " . $row["nomeProduto"]. " " . $row["descricao"]. "<br>";
+    }
+}
+?>
 
 <div class="responsive">
   <div class="gallery">
-    <a target="_blank" href="img_fjords.jpg">
-      <img src="img_fjords.jpg" alt="Trolltunga Norway" width="300" height="200">
+      <img src="images/FOTA2.png" width="300" height="200">
     </a>
-    <div class="desc">DESCRICAO</div>
+    <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo '.$row[descricao]'?></div>
   </div>
 </div>
+
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA3.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA4.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA5.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA6.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA7.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA8.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+<div class="responsive">
+  <div class="gallery">
+      <img src="images/FOTA9.png" width="300" height="200">
+    </a>
+     <div class="desc"><?php echo .$row["nomeProduto"]?></div>
+    <div class="desc"><?php echo .$row["descricao"]?></div>
+  </div>
+</div>
+
 
 
 
@@ -35,5 +112,7 @@ $consulta = "SELECT * FROM fotos where data='$datas' and status='Sim' order by i
 </body>
 </html> 
 
+
+ 
 
  
