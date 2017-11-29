d02db4de2244a35122a0d083dfd73b584066c806
Samirah Sessim <32516708+sessimsamirah@users.noreply.github.com>
2017-11-29 16:04:57 -0200

    pagina de orçamento

---
+++ orcamento.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.green-light_green.min.css" />
+<link rel="stylesheet" href="estilo_galeria.css"/>
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> 
+
+<?php
+include 'header.php';
+include 'head.php';
+?> 
+<?php 
+include 'contato.php';
+?>
+<?php
+include 'footer.php';
+?>
